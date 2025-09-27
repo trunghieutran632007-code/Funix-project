3c89caf547e97ca3cb1bb3c581d1725e62392eb1
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2025-09-27 12:25:15 +0700

    2025 09 27 sap xong yc 2

--- src/Thuat_toan.java
+++ src/Thuat_toan.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Thuat_toan {
@@ -23,5 +26,33 @@ public class Thuat_toan {
 
 
 
+    }
+    public static void readFile(){
+        try{
+            File file = new File("text.txt");
+            Scanner scanner = new Scanner(file);
+
+            ArrayList<Float> list = new ArrayList<>();
+
+            while (scanner.hasNextFloat()) {
+                list.add(scanner.nextFloat());
+            }
+            scanner.close();
+
+            // Chuyển ArrayList -> mảng float[]
+            float[] numbers = new float[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                numbers[i] = list.get(i);
+            }
+
+            // In ra để kiểm tra
+            System.out.println("Dữ liệu trong mảng float:");
+            for (float num : numbers) {
+                System.out.print(num + " ");
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Không tìm thấy file!");
+        }
     }
 }
